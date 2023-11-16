4740f40e89e40bf4a2e2e0fa6541c77a72605686
Alex van den Berg <65889584+Ap3xY@users.noreply.github.com>
2023-11-16 11:57:03 +0200

    Moved assets out of src. Added css to navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
+import styles from "./Navbar.module.css";
+
+import { useState } from "react";
+import { getImageUrl } from "../../utils";
+
 const Navbar = () => {
   return (
-    <nav>
-      <a href="/"></a>
-      <div>
-        <ul>
+    <nav className={styles.Navbar}>
+      <a className={styles.Title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.Menu}>
+        <img
+          className={styles.menuBtn}
+          src={getImageUrl("nav/menuIcon.png")}
+          alt="menu-button"
+        />
+        <ul className={styles.MenuItems}>
           <li>
             <a href="#About">About</a>
           </li>
