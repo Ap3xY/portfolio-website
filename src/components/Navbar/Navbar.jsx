39c0798ef98541109d0327320af9b69ed9e0909f
Alex van den Berg <65889584+Ap3xY@users.noreply.github.com>
2023-11-16 11:25:26 +0200

    Added componets folder and started work on Navbar. Fixed a bug regarding the fonts.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="/"></a>
+      <div>
+        <ul>
+          <li>
+            <a href="#About">About</a>
+          </li>
+          <li>
+            <a href="#Experience">Experience</a>
+          </li>
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
